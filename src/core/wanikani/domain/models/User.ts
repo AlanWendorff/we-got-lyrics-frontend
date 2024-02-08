c92457a43d6d4478620ee7117783dd9c88fcf95c
Alan Wendorff <alanwff4@gmail.com>
2024-02-08 16:39:37 -0300

    removed unused code

--- src/core/wanikani/domain/models/User.ts
+++
@@ -1,4 +0,0 @@
-export type TUser = {
-  username: string;
-  level: number;
-};
--- src/core/wanikani/infrastructure/mappers/user.mapper.ts
+++
@@ -1,7 +0,0 @@
-import { TUser } from '../../domain/models/User';
-import { UserDTO } from '../../../configuration/http/dto/UserDTO';
-
-export const userMapper = (userDTO: UserDTO): TUser => ({
-  username: userDTO.data.username,
-  level: userDTO.data.level
-});
