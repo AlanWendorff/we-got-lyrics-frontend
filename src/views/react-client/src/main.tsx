7e3ee225f820e712ce393ce0668645022cb007d5
Alan Wendorff <alanwff4@gmail.com>
2024-03-09 00:20:16 -0300

    list

--- src/views/react-client/src/main.tsx
+++ src/views/react-client/src/main.tsx
@@ -9,3 +9,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>
 );
+
+// arreglar searchbar & searched list
+// arreglar scroll del contenido en desktop
+// mejorar web screenshot PWA
+// Devolver todas las canciones de un artista
+// Eliminar toolbar del All songs del artista
+// El share en mobile se ve desalineado.
+// Seguir corrigiendo el text overflow en mobile
+// Corregir iconos, que tal vez sean mas simples sin tanto outline
