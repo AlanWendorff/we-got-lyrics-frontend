b159f982f3687db1ede02824784a5f80e345a927
Alan Wendorff <alanwff4@gmail.com>
2024-03-22 15:43:32 -0300

    removed unused props

--- src/views/react-client/src/components/pages/Song/components/Controls/Controls.stories.ts
+++ src/views/react-client/src/components/pages/Song/components/Controls/Controls.stories.ts
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { TSong } from '@core/storeAssets/domain/models/StoreAssets.model';
 import ControlsComponent from './Controls';
 import { FullSongMock } from '@core/configuration/mocks/Song.mock';
 import { Skeleton } from '../../../../../../.storybook/decorators';
@@ -18,7 +17,7 @@ export const Controls: Story = {
   args: {
     isSongStored: true,
     song: FullSongMock,
-    handleSongFav: (song: TSong) => {}
+    handleSongFav: () => {}
   }
 };
 
@@ -26,6 +25,6 @@ export const ControlsComponentSkeleton: Story = {
   args: {
     isSongStored: false,
     song: null,
-    handleSongFav: (song: TSong) => {}
+    handleSongFav: () => {}
   }
 };
